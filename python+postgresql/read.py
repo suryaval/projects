d52a0a7ee21d7ae71b43d64b65676e7e3448f68b
Surya Vallabhaneni <surya.vallabhaneni2@gmail.com>
2016-10-24 14:18:27 -0400

    updated indendation

--- python+postgresql/read.py
+++ python+postgresql/read.py
@@ -4,23 +4,18 @@ import sys
 con = None
 
 try:
-     
-    con = psycopg2.connect(database='album', user='surya', password='Jan@2016')  
-    
-    cur = con.cursor()
-	
+	con = psycopg2.connect(database='album', user='surya', password='Jan@2016')
+	cur = con.cursor()
 	cur.execute("SELECT * FROM Cars")
-
-    rows = cur.fetchall()
-
-    for row in rows:
-        print row
+	rows = cur.fetchall()
+	for row in rows:
+		print(row)
 	
 	
 except psycopg2.DatabaseError as e:
 	if con:
 		con.rollback()
-    print('Error %s' % e)
+	print('Error %s' % e)
 	#print("Error")
 	#sys.exit(1)
     
