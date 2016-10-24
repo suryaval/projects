33b37a8154c33f8d271bb86e8b53ae4520c933fd
Surya Vallabhaneni <surya.vallabhaneni2@gmail.com>
2016-10-24 14:32:32 -0400

    limiting read operation only to rows

---
+++ python+postgresql/read_limited.py
@@ -0,0 +1,30 @@
+import psycopg2
+import sys
+
+
+con = None
+
+try:
+	con = psycopg2.connect(database='album', user='surya', password='Jan@2016')
+	cur = con.cursor()
+	cur.execute("SELECT * FROM Cars")
+	while True:
+		row = cur.fetchone()
+        
+        if row == None:
+			break
+            
+        print row[0], row[1], row[2]
+
+except psycopg2.DatabaseError as e:
+	if con:
+		con.rollback()
+	print('Error %s' % e)
+	#print("Error")
+	#sys.exit(1)
+    
+    
+finally:
+    
+    if con:
+        con.close()
